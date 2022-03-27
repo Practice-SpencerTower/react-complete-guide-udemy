a4a2d2f0f71b00a4e5f756d21ba33d2997b84470
SpencerTower <spencerjtower1@gmail.com>
2022-03-27 15:40:14 -0700

    adds ExpenseDate component, adds styling to ExpenseDate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem/ExpenseItem';
+import expenses from './expenseData';
 
 function App() {
   return (
-    <ExpenseItem/>
+    <>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date} />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date} />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date} />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date} />
+    </>
   );
 }
 
