55ba6f2abe30f7301b1223fea62a28275b0b4698
SpencerTower <spencerjtower1@gmail.com>
2022-03-30 11:40:09 -0700

    adds update title button with handler to ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,25 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import { useState } from 'react';
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2022, 3, 23);
-  // const expenseTitle = 'Car Insurance';
-  // const expenseAmount = 300;
-  return (
 
-    <div className="expense-item">
+  const [title, setTitle] = useState(props.title);
+
+  const changeTitleHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  }
+  return (
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+      <button onClick={changeTitleHandler}>Change Title</button>
+    </Card>
   )
 }
 
