9d4d23647d29bf3ee1c546c55501d577bacda8cb
SpencerTower <spencerjtower1@gmail.com>
2022-03-30 15:45:32 -0700

    clears expense input form after submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -37,6 +37,10 @@ function ExpenseForm() {
     };
 
     console.log('expenseData', expenseData);
+    // reset form input fields
+    setInputTitle('');
+    setInputAmount('');
+    setInputDate('');
   };
 
   return (
@@ -44,15 +48,31 @@ function ExpenseForm() {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input
+            type='text'
+            value={inputTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={inputAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" step="2022-12-31" onChange={dateChangeHandler} />
+          <input
+            type="date"
+            min="2019-01-01"
+            step="2022-12-31"
+            value={inputDate}
+            onChange={dateChangeHandler}
+          />
         </div>
         <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
