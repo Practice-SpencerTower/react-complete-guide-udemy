b98751f97d76ffc9b0cdabd365c8f4a16f9ec357
SpencerTower <spencerjtower1@gmail.com>
2022-04-03 10:41:00 -0700

    adds 'all' option to year filter dropdown, all expenses now displayed by default

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,11 +3,10 @@ import './Expenses.css';
 import Card from "../UI/Card";
 import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
 import { useState } from "react";
-import expenses from "../../expenseData";
 
 function Expenses(props) {
 
-  const [selectedYear, setYear] = useState('');
+  const [selectedYear, setYear] = useState('All');
   const yearFilterHandler = (selectedYear) => {
     setYear(selectedYear);
     console.log('In Expenses year: ', selectedYear);
@@ -20,7 +19,14 @@ function Expenses(props) {
         yearFilterHandler={yearFilterHandler} />
 
       {/* chains filter and map methods: filter based on year, then map filtered items into ExpenseItem */}
-      {
+      {selectedYear === 'All' ?
+        props.expenses.map(expense => {
+          return <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date} />
+        }) :
         props.expenses.filter((expense) => {
           return expense.date.getFullYear().toString() === selectedYear;
         }).map(expense => {
